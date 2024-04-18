8be1876fa011c93f5cfe9d43d60d9b33c7e66327
Stuti Kesharwani <stutik25@gmail.com>
2024-04-18 19:53:08 +0200

    servings-update

--- 18-forkify/starter/src/js/views/paginationView.js
+++ 18-forkify/starter/src/js/views/paginationView.js
@@ -3,34 +3,73 @@ import icons from 'url:../../img/icons.svg';
 
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+
   _generateMarkup() {
+    const curPage = this._data.page;
     const numPages = Mat.ceil(this._data.length / this._data.resultsPerPage);
     console.log(numPages);
 
     //page 1
-    if (this._data.page === 1 && numPages > 1) {
-      return 'page 1, others';
+    if (curPage === 1 && numPages > 1) {
+      return `
+      <button data-goto="${
+        curPage + 1
+      }" class="btn--inline pagination__btn--next">
+      <span>Page ${curPage + 1}</span>
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-right"></use>
+      </svg>
+    </button>
+      `;
     }
 
     //last page
-    if (this._data.page === numPages && numPages > 1) {
+    if (curPage === numPages && numPages > 1) {
       return `
-      <button class="btn--inline pagination__btn--prev">
+      <button data-goto="${
+        curPage - 1
+      }" class="btn--inline pagination__btn--prev">
       <svg class="search__icon">
-        <use href="src/img/icons.svg#icon-arrow-left"></use>
+        <use href="${icons}#icon-arrow-left"></use>
       </svg>
-      <span>Page 1</span>
+      <span>Page ${curPage - 1}</span>
     </button>
       `;
     }
 
     //other page
-    if (this._data.page < numPages) {
-      return 'other page';
+    if (curPage < numPages) {
+      return `
+      <button data-goto="${
+        curPage - 1
+      }" class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-left"></use>
+      </svg>
+      <span>Page ${curPage - 1}</span>
+    </button>
+    <button data-goto="${
+      curPage + 1
+    }" class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-left"></use>
+      </svg>
+      <span>Page ${curPage + 1}</span>
+    </button>
+      `;
     }
 
     // page 1 and there are no other page
-    return 'only page 1';
+    return '';
   }
 }
 
