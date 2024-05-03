0c64a21c46981b6a3020d8fdc51d58a1e335f526
Stuti Kesharwani <stutik25@gmail.com>
2024-05-03 23:16:05 +0200

    final-forkify-project

--- 18-forkify/starter/src/js/views/addRecipeView.js
+++ 18-forkify/starter/src/js/views/addRecipeView.js
@@ -3,10 +3,11 @@ import icons from 'url:../../img/icons.svg';
 
 class AddRecipeView extends View {
   _parentElement = document.querySelector('.upload');
+  _message = 'Recipe was successfully upload :)';
   _window = document.querySelector('.add-recipe-window');
   _overlay = document.querySelector('.overlay');
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
-  _btnClose = document.querySelector('.btn--close-model');
+  _btnClose = document.querySelector('.btn--close-modal');
 
   constructor() {
     super();
